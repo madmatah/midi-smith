959ed0d0382dcb8e7f623fcda8c52febe2bac0e9
Matthieu Huguet <matthieu@huguet.eu>
2026-02-14 15:56:03 +0100

    ✨(dsp) add Switch, GateOpenPredicate and ConstantFilter dsp stages

---
+++ domain/include/domain/dsp/filters/constant_filter.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace domain::dsp::filters {
+
+template <float kValue>
+class ConstantFilter {
+ public:
+  template <typename ContextT>
+  inline float Transform(float sample, const ContextT& ctx) noexcept {
+    (void) sample;
+    (void) ctx;
+    return kValue;
+  }
+
+  inline void Reset() noexcept {}
+};
+
+}  // namespace domain::dsp::filters
---
+++ domain/include/domain/dsp/logic/switch.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <concepts>
+
+#include "domain/dsp/concepts.hpp"
+
+namespace domain::dsp::logic {
+
+template <typename PredicateT, typename TrueStageT, typename FalseStageT>
+class Switch {
+  static_assert(std::default_initializable<TrueStageT>, "TrueStageT must be default-initializable");
+  static_assert(std::default_initializable<FalseStageT>,
+                "FalseStageT must be default-initializable");
+
+ public:
+  template <typename ContextT>
+  inline float Transform(float input, const ContextT& ctx) noexcept {
+    if (PredicateT::Test(ctx)) {
+      return true_stage_.Transform(input, ctx);
+    }
+    return false_stage_.Transform(input, ctx);
+  }
+
+  inline void Reset() noexcept {
+    if constexpr (domain::dsp::concepts::Resettable<TrueStageT>) {
+      true_stage_.Reset();
+    }
+    if constexpr (domain::dsp::concepts::Resettable<FalseStageT>) {
+      false_stage_.Reset();
+    }
+  }
+
+  inline TrueStageT& TrueStage() noexcept {
+    return true_stage_;
+  }
+
+  inline const TrueStageT& TrueStage() const noexcept {
+    return true_stage_;
+  }
+
+  inline FalseStageT& FalseStage() noexcept {
+    return false_stage_;
+  }
+
+  inline const FalseStageT& FalseStage() const noexcept {
+    return false_stage_;
+  }
+
+ private:
+  TrueStageT true_stage_{};
+  FalseStageT false_stage_{};
+};
+
+}  // namespace domain::dsp::logic
