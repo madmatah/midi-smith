8165a363174793b722320f150ff18792864654f1
Matthieu Huguet <matthieu@huguet.eu>
2026-02-24 22:21:44 +0100

    🔨(build) refactor firmware CMakeLists.txt

--- cmake/midismith_firmware_common.cmake
+++ cmake/midismith_firmware_common.cmake
@@ -19,3 +19,20 @@ enable_language(C ASM CXX)
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS ON)
+
+if(NOT HOST_TESTS)
+    list(REMOVE_ITEM CMAKE_C_IMPLICIT_LINK_LIBRARIES ob)
+endif()
+
+function(midismith_firmware_link_map TARGET)
+    target_link_options(${TARGET} PRIVATE -Wl,-Map=${TARGET}.map)
+endfunction()
+
+macro(midismith_add_stm32cubemx TARGET)
+    # CubeMX uses ${CMAKE_PROJECT_NAME} to refer to the firmware target, but when built from the
+    # monorepo root, CMAKE_PROJECT_NAME resolves to the root project name ("midi-smith-monorepo").
+    # Shadowing it here ensures the CubeMX-generated CMakeLists.txt targets the correct executable,
+    # regardless of whether the package is built standalone or from the monorepo root.
+    set(CMAKE_PROJECT_NAME "${TARGET}")
+    add_subdirectory(cmake/stm32cubemx)
+endmacro()
--- firmwares/main-board/cmake/gcc-arm-none-eabi.cmake
+++ firmwares/main-board/cmake/gcc-arm-none-eabi.cmake
@@ -1,53 +1,6 @@
-set(CMAKE_SYSTEM_NAME               Generic)
-set(CMAKE_SYSTEM_PROCESSOR          arm)
+set(MIDISMITH_TARGET_FLAGS "-mcpu=cortex-m7 -mfpu=fpv5-d16 -mfloat-abi=hard")
+set(MIDISMITH_RELEASE_C_FLAGS "-Os -g0")
+set(MIDISMITH_RELEASE_CXX_FLAGS "-Os -g0")
+get_filename_component(MIDISMITH_LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../STM32H7B0XX_FLASH.ld" ABSOLUTE)
 
-set(CMAKE_C_COMPILER_ID GNU)
-set(CMAKE_CXX_COMPILER_ID GNU)
-
-# Some default GCC settings
-# arm-none-eabi- must be part of path environment
-set(TOOLCHAIN_PREFIX                arm-none-eabi-)
-
-set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
-set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_LINKER                    ${TOOLCHAIN_PREFIX}g++)
-set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
-set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
-
-set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
-set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
-set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
-
-set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
-
-# MCU specific flags
-set(TARGET_FLAGS "-mcpu=cortex-m7 -mfpu=fpv5-d16 -mfloat-abi=hard ")
-
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TARGET_FLAGS}")
-set(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS} -x assembler-with-cpp -MMD -MP")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -fdata-sections -ffunction-sections")
-
-option(FORCE_DEBUG_SYMBOLS "Force debug symbols even in Release mode" OFF)
-
-if(FORCE_DEBUG_SYMBOLS)
-    set(RELEASE_DEBUG_FLAGS "-g3")
-else()
-    set(RELEASE_DEBUG_FLAGS "-g0")
-endif()
-
-set(CMAKE_C_FLAGS_DEBUG "-Og -g3")
-set(CMAKE_C_FLAGS_RELEASE "-Os -g0")
-set(CMAKE_CXX_FLAGS_DEBUG "-Og -g3")
-set(CMAKE_CXX_FLAGS_RELEASE "-Os -g0")
-
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
-
-get_filename_component(MAIN_LINKER_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/../STM32H7B0XX_FLASH.ld" ABSOLUTE)
-
-set(CMAKE_EXE_LINKER_FLAGS "${TARGET_FLAGS}")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T \"${MAIN_LINKER_SCRIPT}\"")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --specs=nano.specs")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--print-memory-usage")
-set(TOOLCHAIN_LINK_LIBRARIES "m")
+include(${CMAKE_CURRENT_LIST_DIR}/../../../cmake/toolchain/gcc-arm-none-eabi-base.cmake)
