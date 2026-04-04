7079b70b0f3696fc65b1ddbb83f6ec66b289331a
Matthieu Huguet <matthieu@huguet.eu>
2026-04-04 17:39:58 +0200

    🔧(adc-board) increase CAN task stack size

--- firmwares/adc-board/app/include/app/config/config.hpp
+++ firmwares/adc-board/app/include/app/config/config.hpp
@@ -24,7 +24,7 @@ constexpr uint32_t SENSOR_RTT_TELEMETRY_TASK_STACK_BYTES = 1024;
 
 // CAN
 constexpr uint32_t CAN_RECEIVE_QUEUE_CAPACITY = 16;
-constexpr uint32_t CAN_TASK_STACK_BYTES = 1024;
+constexpr uint32_t CAN_TASK_STACK_BYTES = 2048;
 constexpr uint32_t CAN_TASK_PRIORITY = 2;
 
 // Supervisor
